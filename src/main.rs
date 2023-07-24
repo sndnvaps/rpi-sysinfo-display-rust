ebefb7864fada1148dc51cfc2f0cf1795e9ebb69
Samuel Freeman <sndnvaps@gmail.com>
2023-07-25 00:22:06 +0800

    need to clear_buffer() for the  next oled display info

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,116 @@
-
-
+extern crate alloc;
 extern crate linux_embedded_hal as hal;
 extern crate ssd1306;
-extern crate alloc;
+
+use std::thread;
+use std::time::Duration;
 
 pub mod sysinfo;
 
-#[macro_use] extern crate scan_fmt;
+#[macro_use]
+extern crate scan_fmt;
 use std::error::Error;
 
-
-use local_ipaddress as ip;
-use chrono::Local;
 use alloc::string::ToString;
+use chrono::Local;
+use local_ipaddress as ip;
 
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X12, MonoTextStyleBuilder},
     pixelcolor::BinaryColor,
     prelude::*,
-    text::{Baseline, Text},
     primitives::{Line, PrimitiveStyle},
+    text::{Baseline, Text},
 };
 
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
 use hal::I2cdev;
 
-
 fn main() {
-
     let i2c = I2cdev::new("/dev/i2c-1").unwrap();
     let interface = I2CDisplayInterface::new(i2c);
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
     display.init().unwrap();
 
-    let rpi_ip = ip::get().unwrap();
-    let rpi_time = Local::now().format("%Y-%m-%d %H:%M").to_string();
-    let cpu_temp = sysinfo::get_cpu_temp();
-    let meminfo = sysinfo::get_ram();
-    let cpu_usage = sysinfo::get_sys_cpu_usage().to_string();
+    // just make a loop
+    loop {
+        let rpi_ip = ip::get().unwrap();
+        let rpi_time = Local::now().format("%Y-%m-%d %H:%M").to_string();
+        let cpu_temp = sysinfo::get_cpu_temp();
+        let meminfo = sysinfo::get_ram();
+        let cpu_usage = sysinfo::get_sys_cpu_usage();
+        println!("main loop cpu_use = {}",cpu_usage);
 
-   // println!("MemTotal {} kB,MemFree {} kB",meminfo.total,meminfo.free);
+        // println!("MemTotal {} kB,MemFree {} kB",meminfo.total,meminfo.free);
 
-    let ip_info_show = "IP: ".to_owned() + &rpi_ip;
-    let meminfo_show = "RAM: ".to_owned() + &((meminfo.total - meminfo.free)/1024).to_string() + " / " +  &(meminfo.total/1024).to_string() + " MB";
-    let cpu_info_show = "CPU LOAD: ".to_owned() + &cpu_usage + "% " + &cpu_temp + &"`C";
+        let ip_info_show = "IP: ".to_owned() + &rpi_ip;
+        let meminfo_show = "RAM: ".to_owned()
+            + &((meminfo.total - meminfo.free) / 1024).to_string()
+            + " / "
+            + &(meminfo.total / 1024).to_string()
+            + " MB";
+        let cpu_info_show = format!("CPU: {}% {}`C",cpu_usage,&cpu_temp);
+        println!("cpu_info_show -> {}",cpu_info_show);
 
-    let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X12)
-        .text_color(BinaryColor::On)
-        .build();
+        //let cpu_info_show = "CPU LOAD: ".to_owned() + &cpu_usage + "% " + &cpu_temp + &"`C";
 
-    Text::with_baseline(&rpi_time, Point::new(3,2), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
+        let text_style = MonoTextStyleBuilder::new()
+            .font(&FONT_6X12)
+            .text_color(BinaryColor::On)
+            .build();
 
-    Text::with_baseline(&ip_info_show, Point::new(3, 18), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
+        Text::with_baseline(&rpi_time, Point::new(3, 2), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
 
-    Text::with_baseline(&cpu_info_show, Point::new(3, 32), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
+        Text::with_baseline(&ip_info_show, Point::new(3, 18), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
 
-    Text::with_baseline(&meminfo_show, Point::new(3, 46), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
+        Text::with_baseline(&cpu_info_show, Point::new(3, 32), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
 
-    Line::new(Point::new(0,0),Point::new(127,0))
-        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
-        .draw(&mut display)
-        .unwrap();
+        Text::with_baseline(&meminfo_show, Point::new(3, 46), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
 
-    Line::new(Point::new(0,0),Point::new(0,63))
-        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
-        .draw(&mut display)
-        .unwrap();
+        Line::new(Point::new(0, 0), Point::new(127, 0))
+            .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+            .draw(&mut display)
+            .unwrap();
 
+        Line::new(Point::new(0, 0), Point::new(0, 63))
+            .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+            .draw(&mut display)
+            .unwrap();
 
-    Line::new(Point::new(0,63),Point::new(127,63))
-        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
-        .draw(&mut display)
-        .unwrap();
+        Line::new(Point::new(0, 63), Point::new(127, 63))
+            .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+            .draw(&mut display)
+            .unwrap();
 
-    Line::new(Point::new(127,0),Point::new(127,63))
-        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
-        .draw(&mut display)
-        .unwrap();
+        Line::new(Point::new(127, 0), Point::new(127, 63))
+            .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+            .draw(&mut display)
+            .unwrap();
 
-    Line::new(Point::new(0,16),Point::new(127,16))
-        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
-        .draw(&mut display)
-        .unwrap();
+        Line::new(Point::new(0, 16), Point::new(127, 16))
+            .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+            .draw(&mut display)
+            .unwrap();
 
-    Line::new(Point::new(0,15),Point::new(127,15))
-        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
-        .draw(&mut display)
-        .unwrap();
+        Line::new(Point::new(0, 15), Point::new(127, 15))
+            .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+            .draw(&mut display)
+            .unwrap();
 
-    display.flush().unwrap();
+        display.flush().unwrap();
+        //need to clear_buffer() after flush
+        display.clear_buffer();
 
-    loop {}
+        thread::sleep(Duration::from_secs(10));
+    }
 }
--- src/sysinfo.rs
+++ src/sysinfo.rs
@@ -1,18 +1,18 @@
-use std::process::{Command,Stdio};
+use execute::Execute;
+use std::process::{Command, Stdio};
 use std::thread;
 use std::time::Duration;
-use execute::Execute;
 extern crate alloc;
 
 //rust f32 custom decimal point length pick up from
 //https://zhuanlan.zhihu.com/p/466389032
 trait F32Utils {
-    fn round_fixed(self,n: u32) -> f32;
+    fn round_fixed(self, n: u32) -> f32;
 }
 
 impl F32Utils for f32 {
-    fn round_fixed(self,n: u32) -> f32 {
-        if n<= 0 {
+    fn round_fixed(self, n: u32) -> f32 {
+        if n <= 0 {
             return self.round();
         }
         let i = 10_usize.pow(n) as f32;
@@ -20,17 +20,17 @@ impl F32Utils for f32 {
         if self > 0_f32 {
             // 正数情况下 1.15_f32.round() 为1.2
             let m = x.round() as u32;
-            m as f32/i
+            m as f32 / i
         } else {
             //默认的负数round四舍五入取整(a) -1.15_f32.round() 为 -1.2 (b)
             let mr = x.trunc(); //整数部分
             let mf = x.fract(); //小数部分
             if mf.abs() >= 0.5 {
                 // -3.14159 四舍五入保留3位 则-3141.59 / 1000 = -3.14159(逢五进一) 变为-3.140
-                return (mr+1_f32)/ i;
+                return (mr + 1_f32) / i;
             }
             //小数部分 < 0.5直接舍弃小数部分；小数点直接使用整数部分向前移动n位
-            mr/i
+            mr / i
         }
     }
 }
@@ -39,21 +39,19 @@ pub fn get_cpu_temp() -> String {
     let mut command = execute::command_args!("cat", "/sys/class/thermal/thermal_zone0/temp");
 
     command.stdout(Stdio::piped());
-    
+
     let output = command.execute_output().unwrap();
     let cpu_temp_str = String::from_utf8(output.stdout).unwrap();
     //println!("{}",cpu_temp_str.trim()); trim()用于去除换行符 '\n'
     let cpu_temp = cpu_temp_str.trim().parse::<i32>().unwrap() / 1000;
 
     return cpu_temp.to_string();
-
 }
 
 pub struct MemInfo {
     pub total: u32,
     pub free: u32,
 }
-
 
 struct CpuOccupy {
     user: u32,
@@ -62,23 +60,25 @@ struct CpuOccupy {
     idle: u32,
     iowait: u32,
     irq: u32,
-    softirq: u32
+    softirq: u32,
 }
 
-
-
 pub fn get_ram() -> MemInfo {
-    let mut command = execute::command_args!("head", "-c", "83","/proc/meminfo");
+    let mut command = execute::command_args!("head", "-c", "83", "/proc/meminfo");
 
     command.stdout(Stdio::piped());
-    
+
     let output = command.execute_output().unwrap();
     let meminfo = String::from_utf8(output.stdout).unwrap();
 
-    let (mem_total,mem_free) = scan_fmt!(&meminfo, //input string
-                                                "MemTotal:         {d} kB\nMemFree:          {d} kB", //format
-                                                u32,u32).unwrap(); //types
-    //println!("MemTotal {} kB,MemFree {} kB",mem_total,mem_free);
+    let (mem_total, mem_free) = scan_fmt!(
+        &meminfo,                                             //input string
+        "MemTotal:         {d} kB\nMemFree:          {d} kB", //format
+        u32,
+        u32
+    )
+    .unwrap(); //types
+               //println!("MemTotal {} kB,MemFree {} kB",mem_total,mem_free);
     let mem_info = MemInfo {
         total: mem_total,
         free: mem_free,
@@ -88,15 +88,24 @@ pub fn get_ram() -> MemInfo {
 }
 
 fn get_cpu_occupy() -> CpuOccupy {
-    let mut command = execute::command_args!("head", "-c", "100","/proc/stat");
+    let mut command = execute::command_args!("head", "-c", "100", "/proc/stat");
 
     command.stdout(Stdio::piped());
-    
+
     let output = command.execute_output().unwrap();
     let stat_info = String::from_utf8(output.stdout).unwrap();
-    let (user,nice,system,idle,iowait,irq,softirq) = scan_fmt!(&stat_info, //input string
-                                                "cpu  {d} {d} {d} {d} {d} {d} {d} ", //format
-                                                u32,u32,u32,u32,u32,u32,u32).unwrap(); //types
+    let (user, nice, system, idle, iowait, irq, softirq) = scan_fmt!(
+        &stat_info,                          //input string
+        "cpu  {d} {d} {d} {d} {d} {d} {d} ", //format
+        u32,
+        u32,
+        u32,
+        u32,
+        u32,
+        u32,
+        u32
+    )
+    .unwrap(); //types
     //println!("cpu {} {} {} {} {} {} {} ",user,nice,system,idle,iowait,irq,softirq);
 
     let cpu_info = CpuOccupy {
@@ -106,35 +115,36 @@ fn get_cpu_occupy() -> CpuOccupy {
         idle,
         iowait,
         irq,
-        softirq
+        softirq,
     };
 
-    return cpu_info
+    return cpu_info;
 }
 
-fn cal_cpuoccupy(c1: CpuOccupy,c2: CpuOccupy) -> f32 {
+fn cal_cpuoccupy(c1: CpuOccupy, c2: CpuOccupy) -> f32 {
     let mut cpu_use: f32 = 0.0;
     let od = (c1.user + c1.nice + c1.system + c1.idle + c1.softirq + c1.iowait + c1.irq) as f32; //第一次(用户+优先级+系统+空闲)的时间再赋给od
     let nd = (c2.user + c2.nice + c2.system + c2.idle + c2.softirq + c2.iowait + c2.irq) as f32; //第二次(用户+优先级+系统+空闲)的时间再赋给nd
 
     let id = c2.idle as f32; //用户第一次和第二次的时间之差再赋给id
     let sd = c1.idle as f32; //系统第一次和第二次的时间之差再赋给sd
-    //println!("od = {}, nd = {}, id = {} ,sd = {}",od,nd,id,sd);
-    if (nd - od) != 0.0 {
-        cpu_use =(1_f32 -  (id - sd)/(nd - od))*100.0;
-        println!("cpu_use = {}",cpu_use);
-    } else {
-        cpu_use = 0.0;
+    let sum = nd - od;
+     //println!("od = {}, nd = {}, id = {} ,sd = {}",od,nd,id,sd);
+    if sum != 0.0 {
+        let idle = id - sd;
+        cpu_use = 100_f32 - (idle / sum) * 100.0;
+        cpu_use = cpu_use.round_fixed(2);
+        println!("cpu_use = {}", cpu_use);
     }
 
-    return cpu_use.round_fixed(2);
+    return cpu_use;
 }
 
 pub fn get_sys_cpu_usage() -> f32 {
-    let mut cpu_usage : f32 = 0.0;
+    let mut cpu_usage: f32 = 0.0;
     let cpu_stat1 = get_cpu_occupy();
     thread::sleep(Duration::from_secs(1));
     let cpu_stat2 = get_cpu_occupy();
-    cpu_usage = cal_cpuoccupy(cpu_stat1,cpu_stat2);
+    cpu_usage = cal_cpuoccupy(cpu_stat1, cpu_stat2);
     return cpu_usage;
 }
